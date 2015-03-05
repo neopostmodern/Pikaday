663cdc6a29e0b298ef7c3fa22f68bd5381b02205
neo post modern <clemens@neopostmodern.com>
2015-03-05 15:02:50 +0100

    renamed. removed styles with stubs.

--- package.js
+++ package.js
@@ -1,13 +1,14 @@
 Package.describe({
 	summary: "Pikaday JavaScript Datepicker for Meteor — lightweight, no dependencies, modular CSS",
   	version: "1.0.0",
-  	git: "https://github.com/richsilv/Pikaday.git"
+		name: "neopostmodern:pikaday",
+  	git: "https://github.com/neopostmodern/Pikaday.git"
 });
 
 Package.on_use(function (api) {
- 	api.versionsFrom("METEOR@0.9.1");
+ 	api.versionsFrom("0.9.0");
 	api.use('jquery', 'client');
-	api.use('mrt:moment@2.8.1', 'client');	
+	api.use('momentjs:moment', 'client');
 
 	api.add_files('client/pikaday.css', 'client');
 	api.add_files([
